1a6f1f70e00c4bf17ad6a9104596b391954c2187
Vladimiro Bellini <vbellini@mail.austral.edu.ar>
2026-01-04 20:24:02 -0300

    fix map provider

--- map.js
+++ map.js
@@ -111,9 +111,10 @@ async function initMap() {
         worldCopyJump: true
     });
 
-    // Dark Basemap (CartoDB Dark Matter / Stadia / Alidade)
-    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-        attribution: '© <a href="https://stadiamaps.com/">Stadia Maps</a>',
+    // Dark Basemap (CartoDB Dark Matter) - Free & No API Key necessary for this usage
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
         maxZoom: 20
     }).addTo(map);
 
